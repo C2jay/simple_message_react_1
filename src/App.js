af364b7bb25a1b937b2d6f3cc259684225891950
C2jay <cat.crane15@gmail.com>
2020-09-12 10:29:49 +0800

    simplemessage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // const message = "JavaScript is so cool!"
+  const [message, setMessage] = React.useState('JavaScript is so cool!');
+  
+  function handleClick() {
+    setMessage("My new message!");
+  }  
+
+
+return (
+  <div>
+    <h1>{message}</h1>
+    <button onClick={handleClick}>Update the message!</button>
+  </div>
   );
 }
 
